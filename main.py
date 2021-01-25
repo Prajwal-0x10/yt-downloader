f711efc9926fe0bdff9f8d809e7e12f25382b389
an3rdone <77953179+an3rdone@users.noreply.github.com>
2021-01-25 11:37:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from pytube import YouTube
 
 
 root=Tk()
-root.geometry('400x400')
+root.geometry('400x200')
 root.resizable(0,0)
 root.title('Youtube Downloader')
 Label(root,text = 'Youtube Downloader', font ='arial 20 bold').pack()
+Label(root,text = ':- an3rdone', font ='arial 10 ').pack()
 link =StringVar()
-Label(root, text = 'Paste Link Here:', font = 'arial 15 bold').place(x= 160 , y = 60)
+Label(root, text = 'Link Here:', font = 'arial 15',).place(x= 30, y = 60)
+
 link_enter = Entry(root, width = 50,textvariable = link).place(x = 32, y =90 )
 
 def Downloader():
@@ -16,5 +18,5 @@ def Downloader():
     video=url.streams.first()
     video.download()
     Label(root,text='Download',font='arial 15').place(x=180,y=210)
-Button(root, text='DOWNLOAD', font='arial 15 bold', bg='blue', padx=2, command=Downloader).place(x=180,y=150)
+Button(root, text='DOWNLOAD', font='arial 15 bold', bg='blue', padx=2, command=Downloader).place(x=130,y=150)
 root.mainloop()
